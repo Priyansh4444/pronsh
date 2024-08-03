4581748457d3ee0ac08170095aea8e27a027c450
Priyansh Shah <Priyanshpokemon@gmail.com>
2024-08-03 06:28:17 +0530

    adding analytics for vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Comfortaa } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { Toaster } from "@/components/ui/toaster";
-
+import { Analytics } from '@vercel/analytics/react';
 const inter = Comfortaa({
   subsets: ["latin"],
   style: ['normal'],
@@ -26,6 +26,7 @@ export default function RootLayout({
 
         {children}
         <Toaster />
+        <Analytics mode={'production'} />;
       </body>
     </html>
   );
